3e91a0eefb0b0399e9ef4c590dbe5b1a16ca3d20
Sean Ye <seanye24@gmail.com>
2022-03-17 20:22:04 -0700

    fix project grid

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from "./Footer"
 import Navbar from "./Navbar"
 import "../index.css"
+import { useEffect, useState } from "react"
 
 type LayoutProp = {
   page: "/" | "/projects" | "/about"
@@ -8,9 +9,16 @@ type LayoutProp = {
 
 const Layout: React.FC<LayoutProp> = ({ children, page }) => {
   if (typeof window === "undefined") return null
+  const [height, setHeight] = useState(window.innerHeight)
+
+  useEffect(() => {
+    const handleResize = () => setHeight(window.innerHeight)
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
 
   return (
-    <div className="app" style={{ height: window.innerHeight }}>
+    <div className="app" style={{ height }}>
       <Navbar page={page} />
       {children}
       <Footer />
